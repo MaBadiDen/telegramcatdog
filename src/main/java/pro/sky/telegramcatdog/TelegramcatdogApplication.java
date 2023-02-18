04adf2c0dbd8c4e5a69ff8bec6cb1c351115c984
MaBadiDen <kaba4ok.den@gmail.com>
2023-02-18 22:35:29 +0300

    Initial commit

---
+++ src/main/java/pro/sky/telegramcatdog/TelegramcatdogApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegramcatdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramcatdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramcatdogApplication.class, args);
+	}
+
+}
