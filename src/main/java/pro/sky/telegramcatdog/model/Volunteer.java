0a1f226d80f139e96b78e666eeaa8102bf46a724
Tamerlan <tamerlanishmaev@mail.ru>
2023-02-23 20:18:59 +0300

    added data to application.properties

---
+++ src/main/java/pro/sky/telegramcatdog/model/Volunteer.java
@@ -0,0 +1,12 @@
+package pro.sky.telegramcatdog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+public class Volunteer {
+    public Volunteer() {
+    }
+
+}
