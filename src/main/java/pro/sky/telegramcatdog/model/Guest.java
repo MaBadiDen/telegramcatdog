e2f59465ff16b380a603d4d74f35f7b671425668
MaBadiDen <kaba4ok.den@gmail.com>
2023-03-04 10:12:41 +0300

    Rework two tables - adopters and guests

--- src/main/java/pro/sky/telegramcatdog/model/Guest.java
+++ src/main/java/pro/sky/telegramcatdog/model/Guest.java
@@ -14,45 +14,20 @@ public class Guest {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String telegram_id;
-    private Timestamp last_visit;
-    private int last_menu;
+    private long id;
+    private String telegramId;
+    private Timestamp lastVisit;
+    private int lastMenu;
 
     public Guest() {
 
     }
 
-    public int getId() {
-        return id;
-    }
+    public Guest( String telegramId, Timestamp lastVisit, int lastMenu) {
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTelegram_id() {
-        return telegram_id;
-    }
-
-    public void setTelegram_id(String telegram_id) {
-        this.telegram_id = telegram_id;
-    }
-
-    public Timestamp getLast_visit() {
-        return last_visit;
-    }
-
-    public void setLast_visit(Timestamp last_visit) {
-        this.last_visit = last_visit;
-    }
-
-    public int getLast_menu() {
-        return last_menu;
-    }
-
-    public void setLast_menu(int last_menu) {
-        this.last_menu = last_menu;
+        this.telegramId = telegramId;
+        this.lastVisit = lastVisit;
+        this.lastMenu = lastMenu;
     }
 
     @Override
@@ -60,11 +35,43 @@ public class Guest {
         if (this == o) return true;
         if (!(o instanceof Guest)) return false;
         Guest guest = (Guest) o;
-        return getId() == guest.getId() && getLast_menu() == guest.getLast_menu() && Objects.equals(getTelegram_id(), guest.getTelegram_id()) && Objects.equals(getLast_visit(), guest.getLast_visit());
+        return getId() == guest.getId();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getTelegram_id(), getLast_visit(), getLast_menu());
+        return Objects.hash(getId());
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTelegramId() {
+        return telegramId;
+    }
+
+    public void setTelegramId(String telegramId) {
+        this.telegramId = telegramId;
+    }
+
+    public Timestamp getLastVisit() {
+        return lastVisit;
+    }
+
+    public void setLastVisit(Timestamp lastVisit) {
+        this.lastVisit = lastVisit;
+    }
+
+    public int getLastMenu() {
+        return lastMenu;
+    }
+
+    public void setLastMenu(int lastMenu) {
+        this.lastMenu = lastMenu;
     }
 }
