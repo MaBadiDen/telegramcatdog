55ecbba4802bf63766e991fa0447840d85f70a8a
truemabadi <kaba4ok.den@gmail.com>
2023-03-12 17:15:06 +0300

    Add AdoptionDoc tests

--- src/main/java/pro/sky/telegramcatdog/constants/Constants.java
+++ src/main/java/pro/sky/telegramcatdog/constants/Constants.java
@@ -49,4 +49,6 @@ public class Constants {
     public final static String VOLUNTEER_URL = "/pet-shelter/volunteer";
     public final static String BREED_URL = "/pet-shelter/breed";
 
+    public final static String ADOPTIONDOC_URL = "/pet-shelter/docs";
+
 }
