f9165f881297520d9cc51ba0aad6404e9ffd590f
Olga <dochnohi@yandex.ru>
2023-03-26 10:04:25 +0300

    Fixed stage 1 methods

--- src/main/java/pro/sky/telegramcatdog/repository/BranchParamsRepository.java
+++ src/main/java/pro/sky/telegramcatdog/repository/BranchParamsRepository.java
@@ -2,9 +2,13 @@ package pro.sky.telegramcatdog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pro.sky.telegramcatdog.constants.PetType;
 import pro.sky.telegramcatdog.model.BranchParams;
+
+import java.util.Optional;
 
 @Repository
 public interface BranchParamsRepository extends JpaRepository<BranchParams, Integer> {
 
+     Optional<BranchParams> findByPetType(PetType petType);
 }
